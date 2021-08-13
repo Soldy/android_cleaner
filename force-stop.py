30c3510541a5ca8b5507dbad8d805f5aa8cac911
Soldy <4786022+Soldy@users.noreply.github.com>
2021-08-13 23:41:57 +0100

    list separation
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- force-stop.py
+++ force-stop.py
@@ -1,35 +1,22 @@
 import subprocess
-import hashlib
 import array
 import time
 import sqlite3
+import lib.configread as __config
+import lib.list as listC
 from random import choices
 
 
+print(__config.whiteList())
+_list = listC.listClass()
+_list.whiteInit(__config.whiteList())
 
 class cleaner:
     def __init__(self):
-        self.list = []
-        self.last = ''
-        self.hashes = {}
-        self.sizes = []
-        self.packages = []
-        self.safe_list = []
-        self.priority_list = []
-        self.black_list = ['org.pocketworkstation.pckeyboard']
-        self.killed_list=[]
-        self.killSize=[]
         self.listPackages()
         self.getList()
-        print(self.list)
-        print(self.safe_list)
-        print(self.priority_list)
-    def hash(self):
-        return hashlib.sha3_512(
-            ''.join(self.list).encode('utf8')
-        ).hexdigest()
     def listPackages(self):
-        self.packages = []
+        _list.packageClean()
         proc = subprocess.Popen(
             'adb shell pm list packages',
             shell=True,
@@ -39,11 +26,10 @@ class cleaner:
         for line in proc.stdout.readlines():
             line = line.decode('utf-8')
             parts = line.split(':')
-            self.packages.append(parts[1].rstrip())
-        self.packages = sorted(self.packages)
+            _list.packageAppend(parts[1])
         proc.wait()
     def getList(self):
-        self.list = []
+        _list.appClean()
         proc = subprocess.Popen(
             'adb shell ps',
             shell=True,
@@ -53,37 +39,17 @@ class cleaner:
         for line in proc.stdout.readlines():
             parts = line.split()
             last = parts[len(parts)-1].decode('utf-8')
-            if last in self.packages:
-                self.listAppend(last)
-        self.list = sorted(self.list)
-        self.safeAppend()
-        self.priorityAppend()
+            _list.appAppend(last)
+        _list.safeUpdate()
+        _list.priorityUpdate()
         proc.wait()
-    def listAppend(self, task):
-         if ':' in task:
-             task = (task.split(':'))[0]
-         if task not in self.list:
-            self.list.append(task)
-    def safeAppend(self):
-        self.safe_list = []
-        for app in self.list:
-            if app not in self.black_list:
-               self.safe_list.append(app)
-    def priorityAppend(self):
-        self.priority_list = []
-        for app in self.safe_list:
-            if app not in self.killed_list:
-               self.priority_list.append(app)
-    def killedAppend(self, app):
-        if app not in self.killed_list:
-           self.killed_list.append(app)
-    def killOne(self, task):
+    def killOne(self, app):
         save = {}
         self.getList()
-        has = self.hash()
-        save['before'] = len(self.list)
+        has = _list.appHash()
+        save['before'] = _list.appLen()
         command = (
-           'adb shell am force-stop '+task
+           'adb shell am force-stop '+app
         )
         kill = subprocess.Popen(
            command, 
@@ -92,30 +58,29 @@ class cleaner:
            stderr=subprocess.STDOUT
         )
         kill.wait()
-        self.killedAppend(task)
-        self.last = task
+        _list.killed(app)
         self.getList()
-        save['after'] = len(self.list)
-        if has not in self.hashes:
-            self.hashes[has] = {}
-        self.hashes[has][task]=save
+        save['after'] = _list.appLen()
+        _list.hashAppend(has, app, save)
         return save
     def randomKill(self):
-        if len(self.priority_list) > 0:
-            return self.killOne(choices(self.priority_list)[0])
-        return self.killOne(choices(self.safe_list)[0])
+        if _list.priorityLen() > 0 :
+            return self.killOne(_list.priorityRandom())
+        return self.killOne(_list.safeRandom())
     def checkAndKill(self):
         self.getList()
-        print(self.list)
+        print(_list.appList())
         print(self.randomKill())
-        print(self.last)
-        print(len(self.list))
+        print(_list.killedLast())
+        print(_list.appLen())
     def brutality(self):
         while True:
             self.checkAndKill()
             time.sleep(2)
     def priority(self):
-        while (len(self.priority_list)>0):
+        print(_list.safeLen())
+        print(_list.priorityLen())
+        while (_list.priorityLen()>0):
             self.checkAndKill()
             time.sleep(2)
 
@@ -123,3 +88,4 @@ class cleaner:
 
 test = cleaner()
 test.priority()
+
---
+++ lib/list.py
@@ -0,0 +1,90 @@
+import copy
+import array
+import hashlib
+from random import choices
+
+
+
+class listClass:
+   def __init__(self):
+       self._apps =[]
+       self._hashes = {}
+       self._sizes = []
+       self._packages = []
+       self._safes = []
+       self._priorities = []
+       self._killed = []
+       self._history = []
+       self._whites = []
+   def appClean(self):
+       self.apps = []
+   def appHash(self):
+      return hashlib.sha3_512(
+          ''.join(self._apps).encode('utf8')
+      ).hexdigest()
+   def appLen(self):
+      return len(self._apps)
+   def appList(self):
+      return copy.deepcopy(self._apps)
+   def appAppend(self, app):
+      if ':' in app:
+          app = (app.split(':'))[0]
+      if app not in self._packages:
+          return
+      if app not in self._apps:
+         self._apps.append(copy.deepcopy(app))
+         self._apps = sorted(self._apps)
+   def hashAppend(self, has, app, save):
+      if has not in self._hashes:
+          self._hashes[has] = {}
+      self._hashes[has][app]=save
+   def historyAppend(self, app):
+      self._history.append(copy.deepcopy(app))
+   def killedAppend(self, app):
+      if app not in self._killed:
+          self._killed.append(copy.deepcopy(app))
+   def killedGet(self):
+      return copy.deepcopy(self._killed)
+   def killedLast(self):
+      return copy.deepcopy(
+          self._killed[(len(self._killed)-1)]
+      )
+   def killed(self, app):
+      self.historyAppend(app)
+      self.killedAppend(app)
+   def packageClean(self):
+       self._packages = []
+   def packageAppend(self, app):
+      app = app.rstrip()
+      if app not in self._packages:
+          self._packages.append(copy.deepcopy(app))
+          self._packages = sorted(self._packages)
+   def priorityAppend(self, app):
+      if app not in self._priorities:
+          self._priorities.append(copy.deepcopy(app))
+   def priorityUpdate(self):
+      self._priorities = []
+      for app in self._safes:
+          if app not in self._killed:
+              self.priorityAppend(app)
+   def priorityGet(self):
+      return copy.deepcopy(self._priorities)
+   def priorityLen(self):
+      return len(self._priorities)
+   def priorityRandom(self):
+      return copy.deepcopy(choices(self._priorities)[0])
+   def safeAppend(self, app):
+      if app not in self._safes:
+           self._safes.append(copy.deepcopy(app))
+   def safeUpdate(self):
+      self._safes = []
+      for app in self._apps:
+          if app not in self._whites:
+              self.safeAppend(app)
+   def safeLen(self):
+      return len(self._safes)
+   def safeRandom(self):
+      return copy.deepcopy(choices(self._safes)[0])
+   def whiteInit(self, white):
+       self._whites = copy.deepcopy(white)
+
