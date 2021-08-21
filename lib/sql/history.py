75963f7e1c0e5c9e869f25e42b2a04265e60c40d
Soldy <4786022+Soldy@users.noreply.github.com>
2021-08-21 23:20:36 +0100

    history request
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- lib/sql/history.py
+++ lib/sql/history.py
@@ -23,4 +23,36 @@ class historyDatabase:
            }
        )
        self.con.commit()
+   def check(self, hash_, app_):
+       self.cur.execute(
+           "SELECT * FROM kill_history WHERE hash = :hash AND app = :app ",
+           {
+               "hash":hash_,
+               "app":app_
+           }
+       )
+       if len(self.cur.fetchall()) > 0:
+           return True
+       return False
+   def get(self, hash_, app_):
+       self.cur.execute(
+           "SELECT * FROM kill_history WHERE hash = :hash AND app = :app ",
+           {
+               "hash":hash_,
+               "app":app_
+           }
+       )
+       return self.cur.fetchall()
+   def getApp(self, app_):
+       self.cur.execute(
+           "SELECT * FROM kill_history WHERE app = ? ",
+           app_
+       )
+       return self.cur.fetchall()
+   def get(self, hash_):
+       self.cur.execute(
+           "SELECT * FROM kill_history WHERE hash = ? ",
+           hash_
+       )
+       return self.cur.fetchall()
 
